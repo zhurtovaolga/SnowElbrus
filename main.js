7f20912b9998d134bb9866517a11a05bff8e4b3a
Vladislav <vladostap2003@gmail.com>
2026-06-16 16:45:43 +0300

    Migrating all imported scripts into a single main.js module

---
+++ main.js
@@ -0,0 +1,7 @@
+import './script.js';
+import './sanity_api/intro.js';
+import './sanity_api/author.js';
+import './sanity_api/gallery.js';
+import './sanity_api/prices.js';
+import './sanity_api/registration.js';
+import './sanity_api/importantInfo.js';
